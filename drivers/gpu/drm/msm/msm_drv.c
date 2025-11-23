1c64792db37b3e2cc667dd6b648acb1d21a16aba
Giovanni Ricca <giovanniricca@proton.me>
2025-11-23 23:37:03 +0100

    drm/msm: Move to late_initcall
    Change-Id: Ib5f60dade1feba7f376840e0adb724b43c46f63e
    Signed-off-by: Giovanni Ricca <giovanniricca@proton.me>

--- drivers/gpu/drm/msm/msm_drv.c
+++ drivers/gpu/drm/msm/msm_drv.c
@@ -1316,7 +1316,7 @@ static void __exit msm_drm_unregister(void)
 	msm_dpu_unregister();
 }
 
-module_init(msm_drm_register);
+late_initcall(msm_drm_register);
 module_exit(msm_drm_unregister);
 
 MODULE_AUTHOR("Rob Clark <robdclark@gmail.com");
